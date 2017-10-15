c748fe5cd0637c9ce9b7c64ba76d6fc492765641
JingHuabbs <1904992308@qq.com>
2017-10-15 15:39:40 +0800

    1.4.0 改

--- set/js/style.js
+++ set/js/style.js
@@ -13,6 +13,7 @@ on_off.addEventListener("touchstart",function(ev){
         sw.style.background="#148ad2";
         this.style.right=0;
     }
+    if(n>2) n=1;
 });
 //视频自动播放开关
 var sw01 = document.querySelector(".switch01");
@@ -28,59 +29,82 @@ on_off01.addEventListener("touchstart",function(ev){
         sw01.style.background="#148ad2";
         this.style.right=0;
     }
+    if(m>2) m=1;
 });
 //字体大小
 var sw02 = document.querySelector(".switch02");
-var on_off02=document.querySelector(".on-off02");
+var on_off02=document.querySelector(".on-off02"); //小球
 var new_color=document.querySelector(".new-color");
 var biao=document.querySelector(".no1");
-sw02.addEventListener("touchstart",function(ev){
-    ev.preventDefault();
-    var swl=ev.changedTouches[0].clientX;
-    var l=sw02.offsetWidth;
+var onl=sw02.offsetLeft;
+var onw=sw02.offsetWidth;
     on_off02.addEventListener("touchmove",function(ev){
         ev.preventDefault();
-        var wwe=ev.changedTouches[0].clientX-swl;
-        var onl=on_off02.offsetWidth;
+        var wwe=ev.changedTouches[0].clientX-onl;
         if(wwe<=0) wwe=0;
-        if(wwe>=l) wwe=l;
-        on_off02.style.left=wwe-(onl/2)+"px";
-        new_color.style.width=wwe-(onl/2)+"px";
-        biao.innerHTML=wwe;
-    })
-});
-on_off02.addEventListener("touchend",function(ev){
-    on_off02.touchmove=null;
-});
+        if(wwe>=onw) wwe=onw;
+        on_off02.style.left=Math.floor(wwe)+"px";
+        new_color.style.width=Math.floor(wwe)+"px";
+        biao.innerHTML=Math.floor(Math.floor(wwe)/onw*100);
+    });
+
+
+
+
 //屏幕亮度
 var sw03 = document.querySelector(".switch03");
 var on_off03=document.querySelector(".on-off03");
 var new_color03=document.querySelector(".new-color03");
 var light =document.querySelector(".light");
-var swW=sw03.offsetWidth;
 var arr=[25,50,75,100,"自动"];
-light.addEventListener("touchstart",function(i){
-    for(var i=0;i<arr.length;i++){
-    }
+var s=0;
 
-});
-
-
-sw03.addEventListener("touchstart",function(ev){
-    ev.preventDefault();
-    var sw03l=ev.changedTouches[0].clientX;
-    var sw33l=sw03.offsetWidth;
+    var sw33l=sw03.offsetLeft;
+    var sw33w=sw03.offsetWidth;
     on_off03.addEventListener("touchmove",function(ev){
-        var wwe=ev.changedTouches[0].clientX-sw03l;
+        var wwe=ev.changedTouches[0].clientX-sw33l;
         var onl=on_off03.offsetWidth;
         if(wwe<=0) wwe=0;
-        if(wwe>=sw33l) wwe=sw33l;
-        on_off03.style.left=wwe-(onl/2)+"px";
-        new_color03.style.width=wwe-(onl/2)+"px";
-    })
-});
-on_off03.addEventListener("touchend",function(ev){
-    on_off03.touchmove=null;
+        if(wwe>=sw33w) wwe=sw33w;
+        on_off03.style.left=Math.floor(wwe-(onl/2))+"px";
+        new_color03.style.width=Math.floor(wwe-(onl/2))+"px";
+    });
+
+light.addEventListener("touchstart",function(i){
+    if(s>=arr.length-1){
+        s=0;
+    }else{
+        s++;
+    }
+    var txt = arr[s];
+    switch(s){
+            case 0:
+                on_off03.style.left= sw33w*0.25+"px";
+                new_color03.style.width= sw33w*0.25+"px";
+                light.innerHTML=txt;
+                break;
+            case 1:
+                on_off03.style.left= sw33w*0.5+"px";
+                new_color03.style.width= sw33w*0.5+"px";
+                light.innerHTML=txt;
+                break;
+            case 2:
+                on_off03.style.left= sw33w*0.75+"px";
+                new_color03.style.width= sw33w*0.75+"px";
+                light.innerHTML=txt;
+                break;
+            case 3:
+                on_off03.style.left= sw33w+"px";
+                new_color03.style.width= sw33w+"px";
+                light.innerHTML=txt;
+                break;
+            case 4:
+                on_off03.style.left= sw33w*0.35+"px";
+                new_color03.style.width= sw33w*0.35+"px";
+                light.innerHTML=txt;
+                break;
+        }
+
 });
 
 
